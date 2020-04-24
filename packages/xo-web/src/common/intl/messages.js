e7fa0e8b5ee3b8b80951489bf489a9b2b73b5a02
BenjiReis <benjamin.reis@vates.fr>
2020-04-24 16:39:26 +0200

    better label name

--- packages/xo-web/src/common/intl/messages.js
+++ packages/xo-web/src/common/intl/messages.js
@@ -1120,7 +1120,7 @@ const messages = {
   addRule: 'Add rule',
   deleteRule: 'Delete rule',
   hideRules: 'Hide rules',
-  noAclRuleReason: 'SDN Controller must be loaded',
+  sdnControllerNotLoaded: 'SDN Controller must be loaded',
   showRules: 'Show rules',
   vifAclRules: 'ACL rules',
   vifCreateDeviceButton: 'New device',
